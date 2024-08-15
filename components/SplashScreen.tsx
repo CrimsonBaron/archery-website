7dfc04ea809f670540b6bac7add86d4fb090bb7e
Adam Kišš <contact.adam.kiss@gmail.com>
2024-08-15 09:32:15 +0200

    improved preloader animation

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import React, { useEffect } from "react";
-import { Progress } from "@nextui-org/progress";
 import { motion } from "framer-motion";
 
-import { Logo } from "@/components/icons";
+import LoadingLogo from "./loadingLogo";
+
+//        <Logo className="splash-screen-logo ml-12 md:ml-24 w-28 h-28 md:w-48 md:h-48 lg:w-80 lg:h-80" />
 
 interface ISplashScreenProps {
   callback: () => void;
@@ -15,27 +16,12 @@ const SplashScreen: React.FC<ISplashScreenProps> =
     useEffect(() => {
       setTimeout(() => {
         callback();
-      }, 3000);
+      }, 2000);
     });
 
     return (
-      <motion.div
-        className="w-screen h-screen flex justify-center items-center flex-col gap-8"
-        initial="hidden"
-        transition={{ duration: 0.9 }}
-        variants={{
-          visible: { opacity: 1, scale: 1 },
-          hidden: { opacity: 0, scale: 0.8 },
-        }}
-        whileInView="visible"
-      >
-        <Logo className="spalsh-screen-logo ml-12 md:ml-24 w-28 h-28 md:w-48 md:h-48 lg:w-80 lg:h-80" />
-        <Progress
-          isIndeterminate
-          aria-label="Loading..."
-          className="max-w-md w-48 md:w-96 lg:w-md"
-          size="sm"
-        />
+      <motion.div className="w-screen h-screen flex justify-center items-center flex-col gap-8">
+        <LoadingLogo />
       </motion.div>
     );
   };
---
+++ utils/index.ts
@@ -0,0 +1,8 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
+const animator = gsap;
+
+export default animator;
